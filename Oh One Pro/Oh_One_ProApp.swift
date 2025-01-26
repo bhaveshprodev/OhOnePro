5ecca22bf3a1349a162c538edc03d45bcce2b053
Daniel Nguyen <long@podzim.co>
2025-01-26 20:48:31 +0700

    Initial Commit

---
+++ Oh One Pro/Oh_One_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oh_One_ProApp.swift
+//  Oh One Pro
+//
+//  Created by Daniel Nguyen on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Oh_One_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Oh One ProTests/Oh_One_ProTests.swift
@@ -0,0 +1,17 @@
+//
+//  Oh_One_ProTests.swift
+//  Oh One ProTests
+//
+//  Created by Daniel Nguyen on 1/26/25.
+//
+
+import Testing
+@testable import Oh_One_Pro
+
+struct Oh_One_ProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
